a17f99e255bffd6be4545511e5c31d9a4635c082
Ryan Biondo <biondoryan.dev@gmail.com>
2024-01-08 06:43:14 -0700

    Create New Game Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import '@radix-ui/themes/styles.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link href="/newgame">New Game</Link>
     </div>
   );
 }
